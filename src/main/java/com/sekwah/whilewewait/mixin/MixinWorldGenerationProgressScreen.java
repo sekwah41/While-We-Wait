496783bb23ceb45e702a3dc7dd8a4f5a66f817a5
KabanFriends <45000995+KabanFriends@users.noreply.github.com>
2021-08-12 08:20:15 +0900

    feat: Update to 1.17.1 (#6)
    * Update to 1.17.1
    
    * change the version back for pull request

--- src/main/java/com/sekwah/whilewewait/mixin/MixinWorldGenerationProgressScreen.java
+++ src/main/java/com/sekwah/whilewewait/mixin/MixinWorldGenerationProgressScreen.java
@@ -1,14 +1,14 @@
 package com.sekwah.whilewewait.mixin;
 
 import com.sekwah.whilewewait.audio.AudioManager;
-import net.minecraft.client.gui.WorldGenerationProgressScreen;
+import net.minecraft.client.gui.screen.LevelLoadingScreen;
 import net.minecraft.client.gui.WorldGenerationProgressTracker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(WorldGenerationProgressScreen.class)
+@Mixin(LevelLoadingScreen.class)
 public class MixinWorldGenerationProgressScreen {
 
     @Inject(method = "<init>", at = @At("RETURN"))
@@ -17,7 +17,7 @@ public class MixinWorldGenerationProgressScreen {
     }
 
     @Inject(method = "drawChunkMap", at = @At("HEAD"), cancellable = true)
-    private static void drawChunkMap(WorldGenerationProgressTracker worldGenerationProgressTracker_1, int int_1, int int_2, int int_3, int int_4, CallbackInfo ci) {
+    private static void drawChunkMap(net.minecraft.client.util.math.MatrixStack matrixStack, WorldGenerationProgressTracker worldGenerationProgressTracker_1, int int_1, int int_2, int int_3, int int_4, CallbackInfo ci) {
         ci.cancel(); // cancel drawing that weird rectangle
     }
 }
